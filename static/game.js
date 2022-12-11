e43f6c2311f6e2de230340db71585933f31273e9
Dodo11742 <kabitamsahoo@gmail.com>
2022-12-11 01:01:55 -0500

    Added functional timer

--- static/game.js
+++ static/game.js
@@ -1,7 +1,12 @@
 // Declaring elements
 var table = document.querySelector(".table")
+var opponentTimer = document.querySelector("#opponent-timer")
+var userTimer = document.querySelector("#user-timer")
+// adding move count to keep track of the moves in a side table
 var moveCount = 0
-var moveList = {}
+
+// determining whose move it is to update timers
+var whoseMove = "white"
 //Making Board
 var board = null
 var game = new Chess()
@@ -46,6 +51,7 @@ function onDrop(source, target, piece) {
   // illegal move
   if (move === null) return 'snapback'
 
+  // Getting pgn for notation
   var pgn = game.pgn()
   pgn = pgn.split(".")
   console.log()
@@ -55,20 +61,63 @@ function onDrop(source, target, piece) {
 
   // If white moved
   if (piece[0] == "w") {
+    // updating whose move it is
+    whoseMove = "black"
+
+    // Updating moveCount
     moveCount++
-    // insert notation
+
+    // inserting notation
     var notation = pgn[moveCount].split(" ")[1]
     $(table).find('tbody').append("<tr><td>" + moveCount.toString() + "." + "</td> <td>" + notation + "</tr>");
+
+    //ticking Black's timer with an interval of 1 second
+    var blackTimer = setInterval(function () {
+      var time = countdown(opponentTimer.innerText)
+      
+      // If white's time should be ticking
+      if (whoseMove === "white" || time === "00:00") {
+
+        // Stop black's time from ticking
+        clearInterval(blackTimer)
+      }
+      // Update the htmlText
+      opponentTimer.innerText = time
+    }, 1000);
   }
   // If black moved
   else {
-    // insert
+    // Updating whose move it is
+    whoseMove = "white"
+    // inserting Moves
+    // Getting the all rows
     var rows = document.querySelectorAll("tr")
+    
+    //Getting last row
     var row = rows[rows.length - 1]
+
+    // Adding a cell to the last row
     var newCell = row.insertCell(-1)
+
+    // Joining the notationm to the last row
     var notation = pgn[moveCount].split(" ")[2]
     newCell.appendChild(document.createTextNode(notation));
     console.log(row)
+
+    // ticking white's timer
+    var whiteTimer = setInterval(function () {
+      var time = countdown(userTimer.innerText)
+
+      // if Black's time should be ticking
+      if (whoseMove === "black" || time === "00:00") {
+
+        // Stop this timer
+        clearInterval(whiteTimer)
+      }
+
+      // Updating timer
+      userTimer.innerText = time
+    }, 1000);
   }
 
 }
@@ -100,6 +149,58 @@ function onSnapEnd() {
   board.position(game.fen())
 }
 
+// Timer Updating Functions
+
+// Counting down
+function countdown(time) {
+  // Getting the mins
+  var min = parseInt(time.split(":")[0])
+
+  // Getting the secs
+  var sec = parseInt(time.split(":")[1])
+
+  // Calculating total seconds
+  var totalSecs = min * 60 + sec
+
+  // Looping total seconds amount of time
+  for (var elapsed = 0; elapsed < totalSecs; elapsed++) {
+    // If seconds less than 1
+    if (sec < 1) {
+      // Substract from min instead
+      min -= 1
+      // Set sec to 59
+      sec = 59
+    }
+    // If sec can still be decremented
+    else {
+      // do it
+      sec -= 1
+    }
+    // If reached the end
+    if (timefy(min.toString()) + ":" + timefy(sec.toString()) == "00:00") {
+      // return the end
+      return "00:00"
+    }
+    // Return formatted time-string
+    return timefy(min.toString()) + ":" + timefy(sec.toString())
+  }
+}
+// Formatting time numbers
+function timefy(number) {
+  // If single digit 
+  if (number.length === 1) {
+    // Add a "0" in front and return
+    return "0" + number
+  }
+  // If greater than single digit
+  else {
+    // return as is
+    return number
+  }
+}
+
+// Game Config
+
 var config = {
   draggable: true,
   position: 'start',
@@ -110,6 +211,4 @@ var config = {
   onSnapEnd: onSnapEnd
 }
 board = Chessboard('board', config)
-
-// Moves Updating
 
