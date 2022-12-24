1c9522c1c38f2afd7440e404713ed0cca5907ca6
Dodo11742 <kabitamsahoo@gmail.com>
2022-12-24 18:22:50 -0500

    2 MAJOR CHANGES AND 1 MINOR CHANGE
    MINOR CHANGE
    
    Added pfps
    
    MAJOR CHANGES
    
    ACCOUNT SETTINGS:
    
    Created acount settings page which includes the ability to - 1. Change passwords  2. Change username  3.Change pfp
    
    PROFILE PAGE:
    
    Created profile page which allows you to look at your profile and change your about text

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+
 import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import strip_eval, cap, attempt
-from flask_socketio import SocketIO, send, join_room, leave_room
+from helpers import strip_eval, cap, attempt, save_pfp
+from flask_socketio import SocketIO, send, join_room
 from flask_ngrok import run_with_ngrok
 
 # Configure application
@@ -26,19 +27,22 @@ run_with_ngrok(app)
 
 # Connecting to database
 db = "C:/Users/Dodo/Desktop/Projects/games.db"
+
 # -------------------------------------SOCKETIO-----------------------------------------------------------------#
 socketio = SocketIO(app)
 
 # When aborted
 @socketio.on("abort", namespace="/play")
 def abort():
-    socketio.emit("abort", namespace="/play",to=session["curr_game"]["game_id"])
+    socketio.emit("abort", namespace="/play", to=session["curr_game"]["game_id"])
+
 
 # When a player has resigned
 @socketio.on("resign", namespace="/play")
 def resign():
     # Send back resign event
-    socketio.emit("resign", namespace="/play",to=session["curr_game"]["game_id"])
+    socketio.emit("resign", namespace="/play", to=session["curr_game"]["game_id"])
+
 
 # When connected to /play
 @socketio.on("connect", namespace="/play")
@@ -56,7 +60,13 @@ def connect(data):
 @socketio.on("moved", namespace="/play")
 def message(data):
     # Send the moved event back
-    socketio.emit("moved", data, namespace="/play", broadcast=True, to=session["curr_game"]["game_id"])
+    socketio.emit(
+        "moved",
+        data,
+        namespace="/play",
+        broadcast=True,
+        to=session["curr_game"]["game_id"],
+    )
 
 
 # When game is over
@@ -69,10 +79,12 @@ def game_over(gameinfo):
     # If aborted
     if gameinfo["winner"] == "abort":
         # Delete game from db
-        cursor.execute("DELETE FROM ksgame WHERE game_id = ?", (gameinfo["data"]["game_id"],))
+        cursor.execute(
+            "DELETE FROM ksgame WHERE game_id = ?", (gameinfo["data"]["game_id"],)
+        )
         # leave
         return
-        
+
     # If winner is black
     if gameinfo["winner"] == "black":
         # Set loser to white
@@ -123,9 +135,7 @@ def game_over(gameinfo):
     # if loser's rating is below 100 (min)
     if loser_rating - loser_mod < 100:
         # set it to 100
-        cursor.execute(
-        "UPDATE users SET rating = ? WHERE username = ?",
-        (100, loser)),
+        cursor.execute("UPDATE users SET rating = ? WHERE username = ?", (100, loser)),
     # else
     else:
         # Deduct as normal
@@ -220,6 +230,8 @@ def register():
         # Updating user's session with needed info
         session["user_id"] = user_rows[0]["id"]
         session["username"] = user_rows[0]["username"]
+        session["pfp"] = "static\img\pfp/" + user_rows[0]["pfp"]
+        session["about"] = user_rows[0]["about"]
         # Cloding db
         users.close()
         # Flask Message
@@ -260,6 +272,8 @@ def login():
         session["user_id"] = user_rows[0]["id"]
         session["username"] = user_rows[0]["username"]
         session["logged"] = True
+        session["pfp"] = "static\img\pfp/" + user_rows[0]["pfp"]
+        session["about"] = user_rows[0]["about"]
         flash("Logged in Successfully")
         return redirect("/")
 
@@ -552,5 +566,147 @@ def play():
             return redirect("/play/" + str(cursor.lastrowid))
 
 
+# Profile Page
+@app.route("/profile", methods=["GET", "POST"])
+def profile():
+    # If Just visiting
+    if request.method == "GET":
+        # Return the template
+        return render_template("profile.html", logged=attempt(session, "logged"))
+    # If changes applied
+    else:
+        # Get about text
+        about = request.form.get("about")
+        # update session
+        session["about"] = about
+        # COnnect to db
+        users = sqlite3.connect(db)
+        cursor = users.cursor()
+        # Update db
+        cursor.execute(
+            "UPDATE users SET about = ? WHERE id = ?", (about, session["user_id"])
+        )
+        # Commit changes
+        users.commit()
+        # Flask Success message
+        flash("Changes Saved")
+        # Close db
+        users.close()
+        # Redirect to same page
+        return redirect("/profile")
+
+# Account settings page
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    # IF get
+    if request.method == "GET":
+        # Render
+        return render_template("account.html", logged=attempt(session, "logged"))
+    else:
+        # Connect to db
+        users = sqlite3.connect(db)
+        # Cursor object
+        cursor = users.cursor()
+
+        # If pfp change
+        if request.form.get("submit") == "pfp-change":
+            # Get pfp file
+            pfp = request.files.get("pfp")
+            # Save it
+            pfp_file = save_pfp(pfp,app)
+            # Update db
+            cursor.execute("UPDATE users SET pfp = ? WHERE id = ?", (pfp_file, session["user_id"]))
+            # Update Session
+            session["pfp"] = "static\img\pfp/" + pfp_file
+
+            # Commiting changes
+            users.commit()
+
+            # Closing db
+            users.close()
+
+            # Redirect back
+            flash("Profile Picture Updated")
+            return redirect("/account")
+        
+        # If username change
+        if request.form.get("submit") == "name-change":
+            # Checking Passwords
+            # Getting inpted password
+            password = request.form.get("password")
+            # Getting real password
+            selfpass = cursor.execute("SELECT hash FROM users WHERE id = ?", (session["user_id"],)).fetchone()[0]
+
+            # IF passwords did not match
+            if not check_password_hash(selfpass, password):
+                # close db, Send message and redirect
+                users.close()
+                flash("Password incorrect", "alert-danger")
+                return redirect("/account")
+            # If password Matched
+            else:
+                # Update session
+                session["username"] = request.form.get("username")
+
+                # Update db
+                cursor.execute("UPDATE users SET username = ? WHERE id = ?", (request.form.get("username"), session["user_id"]))
+                
+                # Commit changes
+                users.commit()
+
+                # Close db
+                users.close()
+
+                # Flash success message
+                flash("Changed Applied", "alert-success")
+                # redirect
+                return redirect("/account")
+
+        # If password change
+        if request.form.get("submit") == "pass-change":
+            # Get passwords
+            # Getting inputed password
+            password = request.form.get("current-password")
+            # Getting real password
+            selfpass = cursor.execute("SELECT hash FROM users WHERE id = ?", (session["user_id"],)).fetchone()[0]
+
+            # IF passwords did not match
+            if not check_password_hash(selfpass, password):
+                # Close db
+                users.close()
+                # Send message and redirect
+                flash("Password incorrect", "alert-danger")
+                return redirect("/account")
+            # If passwords matched
+            else:
+                # Get the new pass and its confirmation
+                new_pass = request.form.get("new-password")
+                confirm_pass = request.form.get("confirm-password")
+
+                # If confirmation and new pass match
+                if new_pass == confirm_pass:
+                    # Generate password hash
+                    hashed_pass = generate_password_hash(new_pass)
+                    # Update password
+                    cursor.execute("UPDATE users SET hash = ? WHERE id = ?", (hashed_pass, session["user_id"]))
+
+                    # Commit changes
+                    users.commit()
+                    # Close db
+                    users.close()
+                    # Flash message of succession
+                    flash("Password changed", "alert-success")
+                    # Redirect 
+                    return redirect("/account")
+                
+                # If did not match
+                else:
+                    # Close db
+                    users.close()
+                    # Flash message of not succession
+                    flash("Password did not match", "alert-danger")
+                     # Redirect 
+                    return redirect("/account")
 if __name__ == "__main__":
     socketio.run(app)
+
--- helpers.py
+++ helpers.py
@@ -1,21 +1,60 @@
 import re
+import os
+from PIL import Image
+import secrets
 
+# Profile Picture saving
+def save_pfp(pfp, app):
+    # Create unique hex
+    hex = secrets.token_hex(7)
 
+    # Get extention of the image
+    _, f_ext = os.path.splitext(pfp.filename)
+
+    # Create file name
+    filename = hex + f_ext
+
+    # Make path
+    path = os.path.join(app.root_path, 'static/img/pfp', filename)
+
+    # Deciding output size
+    output_size = (150,150)
+    print(output_size)
+    # Opening image with pillow
+    i = Image.open(pfp)
+    # Resizing
+    i.resize(output_size)
+
+    # Saving resized image into path
+    i.save(path)
+
+    return filename
+
+# Removing eval lines
 def strip_eval(x):
+    # remove curly braces and number in them
     x = re.sub("\{.*?\}", "", x)
+    # Remove random eval numbers
     x = re.sub(".\.\.\.", "", x)
+    # Remove random periods
     x = re.sub("\$.", "", x)
+    # Join together to get rid of whitespace
     x = " ".join(x.split())
     return x
 
-
+# Capitalizing
 def cap(x):
     return x.capitalize()
 
 
+# CHecking for login
 def attempt(dictionary, x):
     try:
+        # If logged exitss
         dictionary[x]
+        # user is logged in
         return True
     except:
+        # If logged does not exit
+        # User is not logged in
         return False
