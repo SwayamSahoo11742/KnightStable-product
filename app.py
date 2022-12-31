7bb67470cb697731e103a1661cd6597cb277d648
SwayamSahoo11742 <117121187+SwayamSahoo11742@users.noreply.github.com>
2022-12-31 16:42:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_ngrok import run_with_ngrok
 from config import Config
 import psycopg2
 import os
-db = "postgres://gosegepgoehqhg:e23cf1d5e2e573d889d6a1b1d6e8ff0faefdd30a3f90d048bbb7965934a44c2c@ec2-34-197-84-74.compute-1.amazonaws.com:5432/d6gt830qsdsj7a"
+db = ""
 # App
 app = Flask(__name__)
 # App Config
